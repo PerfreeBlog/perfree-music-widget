f5e74e6cb79bf54a5ceddfaad00db5803c2bdde3
perfree <perfree@126.com>
2022-01-19 13:19:11 +0800

    修复2.2.1失效问题

--- src/main/java/com/musicWidget/MusicWidget.java
+++ src/main/java/com/musicWidget/MusicWidget.java
@@ -4,10 +4,12 @@ import cn.hutool.core.util.StrUtil;
 import com.perfree.commons.OptionCacheUtil;
 import com.perfree.plugin.proxy.HtmlRenderProxy;
 import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class MusicWidget extends HtmlRenderProxy {
     @Override
     public Document editFrontDocument(Document document, HttpServletResponse response, HttpServletRequest request) {

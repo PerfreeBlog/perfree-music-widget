c7d61292dd9d6046a6895b77ae5f74487474f456
“perfree” <perfree@126.com>
2024-09-28 16:07:21 +0800

    update 4.0.1

--- src/main/java/com/musicWidget/MusicWidget.java
+++ src/main/java/com/musicWidget/MusicWidget.java
@@ -13,8 +13,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicWidget extends HtmlRenderProxy {
 
+    private final String OPTION_IDENTIFICATION = "plugin_perfree-music-widget";
+
     @Resource
     private OptionCacheService optionCacheService;
+
 
     @Override
     public Document editFrontDocument(Document document, HttpServletResponse response, HttpServletRequest request) {
@@ -24,10 +27,10 @@ public class MusicWidget extends HtmlRenderProxy {
         document.head().append("<link rel='stylesheet' href='//cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css'>");
         document.head().append("<style>.aplayer.aplayer-fixed,.aplayer.aplayer-fixed .aplayer-lrc{z-index: 9999999}</style>");
         String html = "<meting-js server='netease' type='playlist' id='{}' fixed=\"true\" theme='{}' autoplay='{}' order='{}'> </meting-js>";
-        String id = optionCacheService.getDefaultValue("MUSIC_WIDGET_ID", "60198");
-        String theme = optionCacheService.getDefaultValue("MUSIC_WIDGET_THEME", "#2980b9");
-        String order = optionCacheService.getDefaultValue("MUSIC_WIDGET_ORDER", "list");
-        String autoPlay = optionCacheService.getDefaultValue("MUSIC_WIDGET_AUTOPLAY", "false");
+        String id = optionCacheService.getDefaultValue("MUSIC_WIDGET_ID", OPTION_IDENTIFICATION,"136205421");
+        String theme = optionCacheService.getDefaultValue("MUSIC_WIDGET_THEME", OPTION_IDENTIFICATION,"#2980b9");
+        String order = optionCacheService.getDefaultValue("MUSIC_WIDGET_ORDER", OPTION_IDENTIFICATION,"list");
+        String autoPlay = optionCacheService.getDefaultValue("MUSIC_WIDGET_AUTOPLAY", OPTION_IDENTIFICATION,"false");
         html = StrUtil.format(html, id, theme, autoPlay, order);
         document.body().before(html);
         document.body().append(" <script src='//cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js'></script>");
